23bc5b1ffff270440461f0ff7fea26a8ef3cd690
alexdemenezes <alexmenezes.business@outlook.com>
2022-06-24 13:23:48 -0300

    feat: UsersService getByEmail method

--- backend/src/services/UsersService.js
+++ backend/src/services/UsersService.js
@@ -22,6 +22,15 @@ class UsersService {
     }
     return null;
   }
+
+  async getByEmail(email) {
+    const user = await User.findOne({
+      where: {
+        email,
+      },
+    });
+    return user;
+  }
 }
 
 module.exports = new UsersService();
