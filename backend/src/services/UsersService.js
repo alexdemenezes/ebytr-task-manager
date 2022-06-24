8f3c2625de3ada1b5f070fecf91dca4c82126daf
alexdemenezes <alexmenezes.business@outlook.com>
2022-06-24 13:20:23 -0300

    feat: userService create method

---
+++ backend/src/services/UsersService.js
@@ -0,0 +1,27 @@
+/* eslint-disable class-methods-use-this */
+const HashGenerator = require('../utils/HashGenerator');
+const User = require('../models/User');
+
+class UsersService {
+  async create(userData) {
+    const { username, email, password } = userData;
+    const user = this.getUserByEmail(email);
+    if (!user) {
+      const generator = new HashGenerator(password, 10);
+      const hash = generator.passwordToHash();
+      const [{ insertId }] = await User.create({
+        username,
+        password: hash,
+        email,
+      });
+      return {
+        id: insertId,
+        username,
+        email,
+      };
+    }
+    return null;
+  }
+}
+
+module.exports = new UsersService();
