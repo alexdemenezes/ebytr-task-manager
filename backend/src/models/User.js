0cd2ca822d13087ff197590dadd09e3e02634ff8
alexdemenezes <alexmenezes.business@outlook.com>
2022-06-24 10:53:48 -0300

    feat: create user model

---
+++ backend/src/models/User.js
@@ -0,0 +1,36 @@
+const { DataTypes } = require('sequelize');
+const db = require('../database');
+
+const User = db.define('users', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  verified: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: false,
+  },
+}, {
+  timestamps: false,
+});
+
+(async () => {
+  await db.sync({ force: false });
+})();
+
+module.exports = User;
