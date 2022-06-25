db43fe5a258c6b67b11193a46fde8a54a1090b34
alexdemenezes <alexmenezes.business@outlook.com>
2022-06-25 19:05:03 -0300

    feat: implements users router  update  methods

--- backend/src/routes/usersRouter.js
+++ backend/src/routes/usersRouter.js
@@ -11,6 +11,27 @@ router.get(
   (req, res) => UsersController.getById(req, res),
 );
 
+router.patch(
+  '/username',
+  (req, res, next) => AuthMiddleware.verifyToken(req, res, next),
+  (req, res, next) => UsersMiddleware.verifyUsername(req, res, next),
+  (req, res) => UsersController.updateUsername(req, res),
+);
+
+router.patch(
+  '/email',
+  (req, res, next) => AuthMiddleware.verifyToken(req, res, next),
+  (req, res, next) => UsersMiddleware.verifyEmail(req, res, next),
+  (req, res) => UsersController.updateEmail(req, res),
+);
+
+router.patch(
+  '/password',
+  (req, res, next) => AuthMiddleware.verifyToken(req, res, next),
+  (req, res, next) => UsersMiddleware.verifyPassword(req, res, next),
+  (req, res) => UsersController.updatePassword(req, res),
+);
+
 router.post(
   '/',
   (req, res, next) => UsersMiddleware.verifyUsername(req, res, next),
